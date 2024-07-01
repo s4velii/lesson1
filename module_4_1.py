cec350aadcfbb84dc7abd9ae79fc9b6f05285c21
s4velii <44133852+s4velii@users.noreply.github.com>
2024-07-01 19:21:53 +0300

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fm
+from true_math import divide as tm
+
+
+result1 = fm(69, 3)
+result2 = fm(3, 0)
+result3 = tm(49, 7)
+result4 = tm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
